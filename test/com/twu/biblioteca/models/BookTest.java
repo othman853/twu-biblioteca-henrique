2219d5bacf2b52732c2596411c23a8ad43ad6058
hscorrea <habyte@gmail.com>
2018-02-26 14:21:15 -0300

    Add test for Book::toString()

--- test/com/twu/biblioteca/models/BookTest.java
+++ test/com/twu/biblioteca/models/BookTest.java
@@ -1,7 +1,14 @@
 package com.twu.biblioteca.models;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class BookTest {
 
+    @Test
+    public void testToString() {
+        Book exampleBook = new Book("Title", "Author", 2018);
+        assertEquals("AUTHOR, Title - 2018", exampleBook.toString());
+    }
 }
