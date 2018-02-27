a37398b0dfec55770b05e7082e37b0b7f4e84de1
hscorrea <habyte@gmail.com>
2018-02-27 19:18:22 -0300

    Add action selection logic

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -5,37 +5,38 @@ import com.twu.biblioteca.models.Library;
 import com.twu.biblioteca.presentation.UserInterface;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class BibliotecaApp {
 
     public static void main(String[] args) {
         ArrayList<Book> bookList = Library.getBookList();
+
+        String equalSignsLine = UserInterface.buildAHorizontalLine('=', 50);
         String hyphensLine = UserInterface.buildAHorizontalLine('-', 50);
-        String asterisksLine = UserInterface.buildAHorizontalLine('=', 50);
 
-        System.out.println(asterisksLine);
+        System.out.println(equalSignsLine);
         System.out.println(UserInterface.buildWelcomeMessage());
-        System.out.println(asterisksLine);
+        System.out.println(equalSignsLine);
+        System.out.println(UserInterface.buildStringForTheMenuOptions());
+        System.out.println(hyphensLine);
 
-        int chosenOption;
+        Scanner input = new Scanner(System.in);
 
-        do {
-            System.out.println(UserInterface.buildStringForTheMenuOptions());
-            System.out.println(hyphensLine);
-            chosenOption = UserInterface.waitForUserChoice();
+        System.out.print("Select your option: ");
+        int chosenOption = input.nextInt();
 
-            switch (chosenOption) {
-                case 1:
-                    System.out.println(hyphensLine);
-                    System.out.println(UserInterface.buildStringForABookList(bookList));
-                    System.out.println(hyphensLine);
-                    break;
-                case 2:
-                    System.out.println("\n" + UserInterface.buildStringForABookList(bookList) + "\n");
-                    Book chosenBook = UserInterface.waitForUserChooseABook(bookList);
-                    chosenBook.checkOut();
-                    break;
-            }
-        } while (chosenOption != 0);
+        switch (chosenOption) {
+            case 1:
+                UserInterface.buildStringForABookList(bookList);
+                break;
+            case 2:
+                UserInterface.buildStringForABookList(bookList);
+                System.out.print("Choose a book: ");// INJECAO DE DEPENDENCIAS
+                Book chosenBook = bookList.get(input.nextInt());
+                chosenBook.checkOut();
+                break;
+
+        }
     }
 }
